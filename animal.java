90d117f1d971a52e01cbf130382f06b3ae9c8f4c
srikanthmupparapu8 <srikanthmupparapu8@gmail.com>
2025-11-15 12:38:17 +0530

    Add files via upload

---
+++ animal.java
@@ -0,0 +1,17 @@
+class Animal { 
+ void sound() {
+  System.out.println("Animal makes a sound");
+  }
+   }
+    class Dog extends Animal {
+      void bark() {
+      System.out.println("Dog barks"); 
+      } 
+      } 
+      public class Main {
+       public static void main(String[] args) {
+        Dog d = new Dog(); 
+        d.sound(); 
+         d.bark(); 
+         } 
+         }
